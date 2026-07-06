1fd0d66ce3e6e1690162245308dfd7add554adb4
Ali <pihepel894@kinws.com>
2026-07-06 16:38:21 +0500

    used while loop

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("It is a class of SMIT")
-console.log("Hello World")
+// console.log("It is a class of SMIT")
+// console.log("Hello World")
+let i = 0 ;
+let foods = ["nihari","biryani","fires","pizza","burger"]
+let tent = " " ;
+while (foods[i]){
+    tent += foods [i];
+    tent += " ";
+    i++ ;
+    
+}
+console.log(tent);
